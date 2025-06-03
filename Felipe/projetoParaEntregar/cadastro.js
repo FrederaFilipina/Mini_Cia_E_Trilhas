9c42dfe394fecc063728b8972fab3e78b78bef12
Felipecardosocosta <felipe.cardoso.costa96@gmail.com>
2025-06-03 17:36:42 -0300

    adicionei partes da versao final

--- Felipe/projetoParaEntregar/cadastro.js
+++ Felipe/projetoParaEntregar/cadastro.js
@@ -1,6 +1,6 @@
-const bancoDados= localStorage
+const bancoDados = localStorage
 
-let cadastroUsuario = JSON.parse(bancoDados.getItem('CadastroUser')) || []
+let cadastroUsuarios = JSON.parse(bancoDados.getItem('CadastroUser')) || []
 
 function entraCadastro(event) {
     event.preventDefault()
@@ -15,13 +15,13 @@ function entraCadastro(event) {
 
     const telefone = document.getElementById("telefDigi")
 
-    const email =document.getElementById("emailDigit")
+    const email = document.getElementById("emailDigit")
 
-    const senha=document.getElementById("senhaDigit")
+    const senha = document.getElementById("senhaDigit")
 
-    const nomeUsuario =document.getElementById("userDigit")
+    const nomeUsuario = document.getElementById("userDigit")
 
-    let sexoUser =descobrirSexo(sexo)
+    let sexoUser = descobrirSexo(sexo)
 
     let cpfValidado = verificarCpf(cpf.value)
 
@@ -30,5 +30,47 @@ function entraCadastro(event) {
     let telefoneValidado = verificarTelefone(telefone.value)
 
     let nomeUsuarioValidado = verificarUsuario(nomeUsuario.value)
-    
+
+}
+
+function verificarCpf(cpf) {
+
+    let pesquisa = cadastroUsuarios.find(cpfBanco => cpfBanco.cpf === cpf)
+    return pesquisa
+}
+function verificarEmail(email) {
+
+    let pesquisa = cadastroUsuarios.find(emailDados => emailDados.email === email)
+
+    return pesquisa
+}
+function verificarTelefone(telef) {
+
+    let pesquisa = cadastroUsuarios.find(telefo => telefo.telefone === telef)
+
+    return pesquisa
+}
+function verificarUsuario(nomeUsuario) {
+
+    let pesquisa = cadastroUsuarios.find(UserName => UserName.nomeUsuario === nomeUsuario)
+
+    return pesquisa
+}
+
+function descobrirSexo(sexo) {
+
+    if (sexo[0].checked) {
+
+        return "Mascolino"
+
+    } else if (sexo[1].checked) {
+
+        return "Feminino"
+    } if (sexo[2].checked) {
+
+        return sexo[3].value
+
+    }
+
+
 }
