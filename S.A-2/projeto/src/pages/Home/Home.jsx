245128087325d674a0e83ce4a555abb58c09a69b
Felipecardosocosta <felipe.cardoso.costa96@gmail.com>
2025-09-18 00:11:41 -0300

    sincronizando

--- S.A-2/projeto/src/pages/Home/Home.jsx
+++ S.A-2/projeto/src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import NavBar from '../../components/NavBar/NavBar'
 
+
+
 function Home() {
+    
+    
+    
   return (
     <div>
       <NavBar/>
+      
     </div>
   )
 }
