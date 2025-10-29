f4aac1cfdd7631ceb14c5f02052ecb1476cd9ec5
Felipecardosodacosta <felipe.cardoso.costa96@gmail.com>
2025-10-28 21:48:29 -0300

    adc

--- S.A-2/api/src/db/connection.js
+++ S.A-2/api/src/db/connection.js
@@ -4,7 +4,7 @@ const { createPool } = require("mysql2/promise")
 const pool = createPool({
     host: "localhost",
     user: 'root',      
-    password: '',    
+    password: 'senai',    
     database:'mini_cia_e_trilhas',
     waitForConnections: true,
     connectionLimit: 10,
