4f65a21cb12eb4975ac44070647ebf6e01df0d01
ronaldgoes <ronaldbentogoes@gmail.com>
2025-11-11 22:17:09 -0300

    Banco de Dados

--- S.A-2/01 - Mapeamento inicial dos requisitos        Mapa de dados/Cod_BD_Ronald.sql
+++ S.A-2/01 - Mapeamento inicial dos requisitos        Mapa de dados/Cod_BD_Ronald.sql
@@ -3,44 +3,45 @@ DROP DATABASE mini_cia_e_trilhas;
 CREATE DATABASE mini_cia_e_trilhas;
 USE mini_cia_e_trilhas;
 
-CREATE TABLE infTrilha(
-id_infTrilha INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(200),
-ponto_partida VARCHAR(200),
-ponto_chegada VARCHAR(200),
-distancia DECIMAL(10,2),
-tempo TIME,
-relevo VARCHAR(200),
-elevacao INT,
-dificuldade INT
-);
 
-INSERT INTO infTrilha
-(nome, ponto_partida, ponto_chegada, distancia, tempo, relevo, elevacao, dificuldade)
+CREATE TABLE trilha(
+    id_trilha INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    regiao VARCHAR (100),
+    nome VARCHAR(200),
+    ponto_partida VARCHAR(200),
+    ponto_chegada VARCHAR(200),
+    distancia DECIMAL(10,2),
+    tempo TIME,
+    relevo VARCHAR(200),
+    elevacao INT,
+    dificuldade INT
+);
+INSERT INTO trilha
+(regiao, nome, ponto_partida, ponto_chegada, distancia, tempo, relevo, elevacao, dificuldade)
 VALUES
-('Trilha da Pedra Azul', 'Domingos Martins', 'Pedra Azul', 8.5, '02:30:00', 'Montanhoso', 450, 3),
-('Trilha da Cachoeira Véu de Noiva', 'Parque do Itatiaia', 'Cachoeira Véu de Noiva', 6.2, '01:45:00', 'Floresta', 180, 2),
-('Trilha do Pico da Bandeira', 'Alto Caparaó', 'Pico da Bandeira', 12.0, '05:00:00', 'Montanhoso', 2890, 5),
-('Trilha do Morro do Careca', 'Natal', 'Topo do Morro', 3.0, '00:50:00', 'Arenoso', 120, 1),
-('Trilha das 7 Quedas', 'Chapada dos Veadeiros', 'Vale das 7 Quedas', 14.8, '06:20:00', 'Cerrado', 520, 4),
-('Trilha do Cambirela', 'Palhoça', 'Cume do Cambirela', 9.0, '04:00:00', 'Montanhoso', 960, 4),
-('Trilha da Lagoa do Peri', 'Florianópolis', 'Lagoa do Peri', 5.5, '01:20:00', 'Floresta', 150, 2),
-('Trilha da Serra do Cipó', 'Santana do Riacho', 'Cachoeira Grande', 7.8, '02:00:00', 'Pedregoso', 340, 3),
-('Trilha da Gruta Azul', 'Bonito', 'Gruta Azul', 4.2, '01:00:00', 'Calcário', 90, 2),
-('Trilha do Pico do Jaraguá', 'São Paulo', 'Pico do Jaraguá', 5.0, '01:40:00', 'Montanhoso', 372, 3);
+('Sul', 'Trilha da Lagoinha do Leste', 'Pântano do Sul', 'Praia da Lagoinha do Leste', 4.20, '01:20:00', 'Costão/Mata Atlântica', 250, 4),
+('Sul', 'Trilha dos Naufragados', 'Caieira da Barra do Sul', 'Praia dos Naufragados', 2.60, '00:50:00', 'Floresta/Costão', 100, 2),
+('Leste', 'Trilha da Costa da Lagoa', 'Canto dos Araçás', 'Comunidade da Costa da Lagoa', 7.50, '02:10:00', 'Mata Atlântica', 150, 3),
+('Leste', 'Trilha do Gravatá', 'Praia Mole (entrada)', 'Praia do Gravatá', 1.00, '00:30:00', 'Costão/Rochoso', 60, 1),
+('Leste', 'Trilha da Galheta', 'Praia Mole', 'Praia da Galheta', 0.80, '00:20:00', 'Areia/Costão', 40, 1),
+('Leste', 'Trilha do Morro da Coroa', 'Mirante da Lagoinha do Leste', 'Topo do Morro da Coroa', 1.20, '00:40:00', 'Montanhoso/Rochoso', 200, 3),
+('Central', 'Trilha do Poção', 'Córrego Grande (Entrada)', 'Cachoeira do Poção', 0.70, '00:20:00', 'Floresta/Mata', 80, 1),
+('Sul', 'Trilha do Morro do Lampião', 'Rua Pau de Canela, Campeche', 'Cume do Morro do Lampião', 2.80, '01:30:00', 'Montanhoso/Rochoso', 180, 2),
+('Leste', 'Trilha da Barra da Lagoa → Piscinas Naturais', 'Barra da Lagoa (início)', 'Piscinas Naturais da Barra da Lagoa', 0.60, '00:15:00', 'Costão/Rochoso', 30, 1),
+('Norte', 'Trilha do Morro do Rapa', 'Praia Brava', 'Praia da Lagoinha do Norte (via Morro do Rapa)', 2.80, '01:30:00', 'Costão/Mata', 150, 2);
+
 
 
 CREATE TABLE usuario(
-id_usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(200) UNIQUE NOT NULL,
-cpf BIGINT(11) UNIQUE NOT NULL,
-dt_nascimento DATE NOT NULL,
-sexo ENUM('Masculino','Feminino') NOT NULL,
-num_celular BIGINT(11) UNIQUE NOT NULL,
-email VARCHAR(200) NOT NULL,
-senha VARCHAR(64) NOT NULL
+    id_usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(200) NOT NULL,
+    cpf BIGINT(11) UNIQUE NOT NULL,
+    dt_nascimento DATE,
+    sexo ENUM('Masculino','Feminino') NOT NULL,       
+    num_celular BIGINT(11) UNIQUE,
+    email VARCHAR(200) UNIQUE NOT NULL,
+    senha VARCHAR(64) NOT NULL
 );
-
 INSERT INTO usuario (nome, cpf, dt_nascimento, sexo, num_celular, email, senha) VALUES
 ('Ana Souza', 12345678901, '1990-05-12', 'Feminino', 11987654321, 'ana.souza@email.com', 'senha123'),
 ('Bruno Lima', 23456789012, '1988-03-22', 'Masculino', 21998765432, 'bruno.lima@email.com', 'bruno2024'),
@@ -54,62 +55,128 @@ INSERT INTO usuario (nome, cpf, dt_nascimento, sexo, num_celular, email, senha) 
 ('João Pereira', 11223344556, '1987-02-17', 'Masculino', 71998901234, 'joao.pereira@email.com', 'joao@321');
 
 
-CREATE TABLE trilha(
-id_trilha INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-dia DATE,
-horario TIME,
-ponto_de_encontro VARCHAR(200),
-vagas INT,
 
-infTrilha_id INT,
-FOREIGN KEY (infTrilha_id)
-REFERENCES infTrilha (id_infTrilha)
+CREATE TABLE evento(
+    id_evento INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dia DATE,
+    horario TIME,
+    ponto_de_encontro VARCHAR(200),
+    vagas INT,
+    condicao ENUM ("Ativo", "Concluido") DEFAULT("Ativo"),
+    trilha_id INT,
+    criador_id INT,
+    FOREIGN KEY (criador_id) REFERENCES usuario (id_usuario),
+    FOREIGN KEY (trilha_id) REFERENCES trilha (id_trilha)
 );
+INSERT INTO evento (criador_id, dia, horario, ponto_de_encontro, vagas, trilha_id) VALUES
+('1','2025-11-06', '08:30:00', 'Entrada do Parque Pedra Azul', 5, 1),
+('2','2025-11-06', '07:00:00', 'Portaria do Itatiaia', 20, 2),
+('3','2025-11-15', '05:00:00', 'Base do Pico da Bandeira', 10, 3),
+('4','2025-11-20', '09:00:00', 'Praia de Ponta Negra', 25, 4),
+('5','2025-11-25', '06:00:00', 'Centro de Visitantes Chapada', 12, 5);
 
-INSERT INTO trilha (dia, horario, ponto_de_encontro, vagas, infTrilha_id) VALUES
-('2025-11-10', '07:00:00', 'Entrada do Parque Pedra Azul', 15, 1),
-('2025-11-12', '08:30:00', 'Portaria do Itatiaia', 20, 2),
-('2025-11-15', '05:00:00', 'Base do Pico da Bandeira', 10, 3),
-('2025-11-20', '09:00:00', 'Praia de Ponta Negra', 25, 4),
-('2025-11-25', '06:00:00', 'Centro de Visitantes Chapada', 12, 5);
 
 
 CREATE TABLE participante(
-id_participante INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-classe ENUM ("C", "P"),
-
-usuario_id INT,
-FOREIGN KEY (usuario_id)
-REFERENCES usuario (id_usuario),
-
-trilha_id INT,
-FOREIGN KEY (trilha_id)
-REFERENCES trilha (id_trilha)
+    id_participante INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT,
+    FOREIGN KEY (usuario_id) REFERENCES usuario (id_usuario),
+    evento_id INT,
+    FOREIGN KEY (evento_id) REFERENCES evento (id_evento)
 );
+INSERT INTO participante (usuario_id, evento_id) VALUES
+(1, 1), (2, 1), (3, 1), (4, 1),
+(5, 2), (6, 2), (7, 2), (8, 2),
+(9, 3), (10, 3), (1, 3), (2, 3),
+(3, 4), (4, 4), (5, 4), (6, 4),
+(7, 5), (8, 5), (9, 5), (10, 5);
 
-INSERT INTO participante (classe, usuario_id, trilha_id) VALUES
-('C', 1, 1), ('P', 2, 1), ('P', 3, 1), ('P', 4, 1),
-('C', 5, 2), ('P', 6, 2), ('P', 7, 2), ('P', 8, 2),
-('C', 9, 3), ('P', 10, 3), ('P', 1, 3), ('P', 2, 3),
-('C', 3, 4), ('P', 4, 4), ('P', 5, 4), ('P', 6, 4),
-('C', 7, 5), ('P', 8, 5), ('P', 9, 5), ('P', 10, 5);
 
-SELECT
-trilha.id_trilha,
-infTrilha.nome,
-trilha.dia,
-trilha.horario,
-trilha.vagas,
-count(participante.id_participante) AS participantes
-FROM infTrilha
+
+
+
+
+-- Comando para CADASTRAR novo Usuário (i)
+INSERT INTO usuario
+(nome, email, cpf, senha, sexo)
+VALUES
+();
+-- Comando para CADASTRAR novo Usuário (i)
+
+-- Comando para VERIFICAR se o LOGIN está correto(i)
+SELECT id_usuario, email, senha FROM usuario
+WHERE email = 'e-mail@email.mail'
+AND senha = 'senha123';
+-- Comando para VERIFICAR se o LOGIN está correto(f)
+
+-- Comando para criar os Cards da página HOME LogOff (i)
+SELECT trilha.nome AS 'Nome da Trilha', evento.dia AS 'Data', evento.horario AS 'Horário', (evento.vagas - COUNT(participante.id_participante)) AS 'Vagas Disp.'FROM evento
 JOIN trilha
-ON trilha.infTrilha_id = infTrilha.id_infTrilha
-JOIN participante
-ON trilha.id_trilha = participante.trilha_id
-GROUP BY trilha.id_trilha;
+ON evento.trilha_id = trilha.id_trilha
+LEFT JOIN participante
+ON participante.evento_id = evento.id_evento
+WHERE condicao = "Ativo"
+GROUP BY trilha.nome, evento.dia, evento.horario, evento.vagas
 
+HAVING (evento.vagas - COUNT(participante.id_participante)) > 0
+AND evento.dia BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 5 DAY);
+-- Comando para criar os Cards da página HOME LogOff (i)
 
+-- Comando para criar os Cards da página AGENDA LogOff/LogIn (i)
+SELECT trilha.nome AS 'Nome da Trilha', evento.dia AS 'Data', evento.horario AS 'Horário', (evento.vagas - COUNT(participante.id_participante)) AS 'Vagas Disp.' FROM evento
+JOIN trilha
+ON evento.trilha_id = trilha.id_trilha
+LEFT JOIN participante
+ON participante.evento_id = evento.id_evento
+WHERE condicao = "Ativo"
+GROUP BY trilha.nome, evento.dia, evento.horario, evento.vagas
 
+HAVING (evento.vagas - COUNT(participante.id_participante)) > 0
+AND CONCAT(evento.dia, ' ', evento.horario) >= NOW()
+ORDER BY evento.dia, evento.horario;
+-- Comando para criar os Cards da página AGENDA LogOff / LogIn (i)
 
+-- Comando para criar os Cards da página TRILHAS LogOff (i)
+SELECT trilha.nome AS 'Nome da Trilha', trilha.distancia AS 'Distância', trilha.tempo AS 'Tempo', trilha.dificuldade AS 'Dificuldade' FROM trilha
+GROUP BY trilha.nome, trilha.distancia, trilha.tempo, trilha.dificuldade;
+-- Comando para criar os Cards da página TRILHAS LogOff (i)
 
+-- Comando para criar os Cards da página TRILHAS LogIn (i)
+SELECT trilha.nome AS 'Nome da Trilha', trilha.ponto_partida AS 'Ponto Inicial', trilha.ponto_chegada AS 'Ponto Final', trilha.distancia AS 'Distância', trilha.tempo AS 'Tempo', trilha.relevo AS 'Tipo do Relevo', trilha.elevacao AS 'Grau de Elevação', trilha.dificuldade AS 'Dificuldade' FROM trilha
+GROUP BY trilha.nome, trilha.ponto_partida, trilha.ponto_chegada, trilha.distancia, trilha.tempo, trilha.relevo, trilha.elevacao, trilha.dificuldade;
+-- Comando para criar os Cards da página TRILHAS LogIn (i)
+
+-- Comando para criar uma adicionar um EVENTO (i)
+INSERT INTO evento (criador_id, dia, horario, ponto_de_encontro, vagas, trilha_id) VALUES();
+-- Comando para criar uma adicionar um EVENTO (i)
+
+-- Comando para alterar informação do EVENTO (i)
+UPDATE evento SET dia = ?, horario = ?, ponto_de_encontro = ?, vagas = ? WHERE id_evento = ? AND criador_id = ?;
+-- Comando para alterar informação do EVENTO (i)
+
+-- Comando para alterar a condicao do EVENTO (i)
+UPDATE evento SET condicao = ? WHERE id_evento = ? AND criador_id = ?;
+-- Comando para alterar a condicao do EVENTO (i)
+
+-- Comando para deletar o EVENTO (i)
+DELIMITER &&
+DELETE FROM participante WHERE evento_id = ?;
+DELETE FROM evento WHERE id_evento = ?;
+&&
+-- Comando para deletar o EVENTO (i)
+
+-- Comando para alterar informação do USUARIO (i)
+UPDATE usuario SET num_celular = ?, email = ? WHERE id_usuario = ? AND senha = ?;
+-- Comando para alterar informação do USUARIO (i)
+
+-- Comando para alterar senha do USUARIO (i)
+UPDATE usuario SET senha = "carro" WHERE id_usuario = 1 AND senha = "senha123";
+-- Comando para alterar senha do USUARIO (i)
+
+-- Comando para deletar o USUARIO (i)
+DELETE FROM usuario WHERE id_usuario= 1 AND senha = "carro";
+DELETE FROM criador_id WHERE 
+-- Comando para deletar o USUARIO (i)
+
+SELECT * FROM usuario
 
