8450f25450f3e9adc7fcdc54d30d88af93d66149
Felipecardosodacosta <felipe.cardoso.costa96@gmail.com>
2025-11-13 21:22:06 -0300

    adc

--- S.A-2/projeto/src/server/alterarDadosUser.jsx
+++ S.A-2/projeto/src/server/alterarDadosUser.jsx
@@ -1,7 +1,25 @@
 
 import React from 'react'
+import axios from 'axios'
 
-const alterarDadosUser = (user) => {
+const alterarDadosUser = async (user,token) => {
+
+    if (!token) {
+
+        return {mensagem:"Erro nessesario token"}
+
+    }
+
+    try {
+
+        const resultado =await axios.put("http://localhost:3000/modificar/senha/user",user,{headers:{authorization: token}})
+        
+        return resultado.data.mensagem
+
+    } catch (error) {
+        
+        return {mensagem:"Erro ao acessar modificação", error}
+    }
     
  
 }
